628ba89b42ead2f06dfb7da750380bfca910b72f
estevo <pedro.cordova2002@gmail.com>
2023-08-05 00:20:17 -0300

    create entity acesso

---
+++ src/main/java/estevao/market/model/Acesso.java
@@ -0,0 +1,53 @@
+package estevao.market.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao; /* Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO ou ROLE_COORDENADOR */
+
+    @Override
+    public String getAuthority() {
+        return this.descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Acesso)) return false;
+        Acesso acesso = (Acesso) o;
+        return Objects.equals(id, acesso.id) && Objects.equals(descricao, acesso.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descricao);
+    }
+}
