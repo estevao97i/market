8d9d70fe10b9822cfd7058fb646e5d906e0a2e77
estevo <pedro.cordova2002@gmail.com>
2023-08-22 19:51:11 -0300

    create table NotaItemProduto

---
+++ src/main/java/estevao/market/model/NotaItemProduto.java
@@ -0,0 +1,73 @@
+package estevao.market.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "nota_item_produto")
+@SequenceGenerator(name = "seq_nota_item_produto", sequenceName = "seq_nota_item_produto", allocationSize = 1, initialValue = 1)
+public class NotaItemProduto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_item_produto")
+    private Long id;
+
+    @ManyToOne(targetEntity = Produto.class)
+    @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private Produto produto;
+
+    @ManyToOne(targetEntity = NotaFiscalCompra.class)
+    @JoinColumn(name = "nota_fiscal_compra_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_compra_fk"))
+    private NotaFiscalCompra notaFiscalCompra;
+
+    @Column(nullable = false)
+    private Double quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public NotaFiscalCompra getNotaFiscalCompra() {
+        return notaFiscalCompra;
+    }
+
+    public void setNotaFiscalCompra(NotaFiscalCompra notaFiscalCompra) {
+        this.notaFiscalCompra = notaFiscalCompra;
+    }
+
+    public Double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NotaItemProduto)) return false;
+        NotaItemProduto that = (NotaItemProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
