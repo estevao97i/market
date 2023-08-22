fbb2569d08f81e68ff7db928be5e769a4816d737
estevo <pedro.cordova2002@gmail.com>
2023-08-22 18:57:25 -0300

    alter table ImagemProduto JPA

--- src/main/java/estevao/market/model/ImagemProduto.java
+++ src/main/java/estevao/market/model/ImagemProduto.java
@@ -3,6 +3,7 @@ package estevao.market.model;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "imagem_produto")
@@ -13,5 +14,58 @@ public class ImagemProduto implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_imagem_produto")
     private Long id;
 
+    @Column(columnDefinition = "text")
+    private String imagemOriginal;
 
+    @Column(columnDefinition = "Text")
+    private String imagemMiniatura;
+
+    @ManyToOne(targetEntity = Produto.class)
+    @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Produto produto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImagemOriginal() {
+        return imagemOriginal;
+    }
+
+    public void setImagemOriginal(String imagemOriginal) {
+        this.imagemOriginal = imagemOriginal;
+    }
+
+    public String getImagemMiniatura() {
+        return imagemMiniatura;
+    }
+
+    public void setImagemMiniatura(String imagemMiniatura) {
+        this.imagemMiniatura = imagemMiniatura;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ImagemProduto)) return false;
+        ImagemProduto that = (ImagemProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
