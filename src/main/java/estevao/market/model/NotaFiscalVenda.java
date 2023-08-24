73974fa94be822caf3301329019ffba414eaa213
estevo <pedro.cordova2002@gmail.com>
2023-08-24 00:15:50 -0300

    upgrade table NotaFiscalVenda

--- src/main/java/estevao/market/model/NotaFiscalVenda.java
+++ src/main/java/estevao/market/model/NotaFiscalVenda.java
@@ -3,6 +3,7 @@ package estevao.market.model;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "nota_fiscal_venda")
@@ -25,6 +26,64 @@ public class NotaFiscalVenda implements Serializable {
     @Column(columnDefinition = "text")
     private String pdf;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(String xml) {
+        this.xml = xml;
+    }
+
+    public String getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(String pdf) {
+        this.pdf = pdf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NotaFiscalVenda)) return false;
+        NotaFiscalVenda that = (NotaFiscalVenda) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
