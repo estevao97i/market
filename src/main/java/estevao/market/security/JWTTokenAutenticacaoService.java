133d55a7e1805c1e6612c2e97e0c29baffd1ba8e
estevo <pedro.cordova2002@gmail.com>
2023-09-16 17:04:40 -0300

    JWT created

--- src/main/java/estevao/market/security/JWTTokenAutenticacaoService.java
+++ src/main/java/estevao/market/security/JWTTokenAutenticacaoService.java
@@ -1,10 +1,16 @@
 package estevao.market.security;
 
+import estevao.market.ApplicationContextLoad;
+import estevao.market.model.Usuario;
+import estevao.market.repository.UsuarioRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
@@ -39,7 +45,63 @@ public class JWTTokenAutenticacaoService {
         // retorna a resposta pra tela e para o usuario (API, navegador, aplicativo)
         response.addHeader(HEADER_STRING, token);
 
+        liberacaoCors(response);
+
         // Usado para o Test API do POSTMAN
         response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+    }
+
+    // Retorna o usuario validado com token ou caso nao seja valido retorna null
+    public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token != null) {
+
+            String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+
+            // Faz a validacao do token do usuario e extrai o usuario de dentro
+            String user = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJwt(tokenLimpo)
+                    .getBody().getSubject(); // Admin ou Estevao
+
+            if (user != null) {
+                Usuario usuario = ApplicationContextLoad
+                        .getApplicationContext()
+                        .getBean(UsuarioRepository.class)
+                        .findUserByLogin(user); // faz a pesquisa no repository e retorna se tem o usuario dentro do banco mesmo
+
+                if (usuario != null) { // se tem o usuario no banco retorna o usuario no response
+                    return new UsernamePasswordAuthenticationToken(
+                            usuario.getLogin(),
+                            usuario.getPassword(),
+                            usuario.getAuthorities());
+                }
+            }
+
+        }
+
+        liberacaoCors(response);
+        return null;
+    }
+
+    // fazendo liberação erro de CORS no navegador
+    private void liberacaoCors(HttpServletResponse response) {
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
     }
 }
