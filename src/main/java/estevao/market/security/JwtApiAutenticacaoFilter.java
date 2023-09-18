e960892161998d9759a9ac77c7d2162a4f1e608b
estevo <pedro.cordova2002@gmail.com>
2023-09-17 22:09:15 -0300

    finished JWT token

--- src/main/java/estevao/market/security/JwtApiAutenticacaoFilter.java
+++ src/main/java/estevao/market/security/JwtApiAutenticacaoFilter.java
@@ -1,4 +1,32 @@
 package estevao.market.security;
 
-public class JwtApiAutenticacaoFilter {
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// Filtro que captura todas as requisicoes para autenticacao
+public class JwtApiAutenticacaoFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        // Estabelece a autenticacao do usuario
+
+        Authentication authentication = new JWTTokenAutenticacaoService().getAuthentication(
+                (HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse);
+
+        // coloca o processo de autenticacao para o spring security
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        chain.doFilter(servletRequest, servletResponse);
+
+    }
 }
--- src/main/java/estevao/market/security/WebConfigSecurity.java
+++ src/main/java/estevao/market/security/WebConfigSecurity.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpSessionListener;
 @Configuration
@@ -19,6 +23,26 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 
     @Autowired
     private ImplementacaoUserDetailService implementacaoUserDetailService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .disable().authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/index").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+        // redireciona ou lança um retorno para o index quando desloga
+                .anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+
+        // mapeia o logout do sistema
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+
+        // filtra as requisições para login de JWT
+                .and().addFilterAfter(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+
+                .addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 
     /* vai consultar o user no banco com Spring Security */
     @Override
@@ -30,9 +54,9 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
     /* liberar URLs para testar dentro da api */
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso", "/updateAcesso")
-                .antMatchers(HttpMethod.POST, "/salvarAcesso")
-                .antMatchers(HttpMethod.DELETE, "/deleteAcesso")
-                .antMatchers(HttpMethod.PUT, "/updateAcesso");
+        //  web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso", "/updateAcesso")
+           //     .antMatchers(HttpMethod.POST, "/salvarAcesso")
+           //     .antMatchers(HttpMethod.DELETE, "/deleteAcesso")
+           //     .antMatchers(HttpMethod.PUT, "/updateAcesso");
     }
 }
