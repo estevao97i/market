8eead55c589cbc48f62990e638db29d488f4ede5
estevo <pedro.cordova2002@gmail.com>
2023-10-11 23:31:58 -0300

    fix - fix the sql queries

--- src/main/java/estevao/market/repository/PessoaJuridicaRepository.java
+++ src/main/java/estevao/market/repository/PessoaJuridicaRepository.java
@@ -20,6 +20,6 @@ public interface PessoaJuridicaRepository extends CrudRepository<PessoaJuridica,
     @Query(" select count(p) from PessoaJuridica p where p.inscEstadual = :inscEstadual ")
     Integer existeInsEstadualCadastrado(String inscEstadual);
 
-    @Query(" Select p from PessoaJuridica p where p.nome like trim(upper(%:nome%)) ")
+    @Query(" Select p from PessoaJuridica p where where trim(upper(p.nome)) like %:nome% ")
     List<PessoaFisica> pesquisaPorNomePJ(String nome);
 }
