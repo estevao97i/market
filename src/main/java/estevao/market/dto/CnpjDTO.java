8f13f202fe9d5c2253c90298a4e38bc4e4b90dd5
estevo <pedro.cordova2002@gmail.com>
2023-10-26 00:01:34 -0300

    feat - add getters and setters

--- src/main/java/estevao/market/dto/CnpjDTO.java
+++ src/main/java/estevao/market/dto/CnpjDTO.java
@@ -36,6 +36,229 @@ public class CnpjDTO implements Serializable {
     private QsaDTO qsa;
     private BillingDTO billing;
 
+    public Date getAbertura() {
+        return abertura;
+    }
+
+    public void setAbertura(Date abertura) {
+        this.abertura = abertura;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFantasia() {
+        return fantasia;
+    }
+
+    public void setFantasia(String fantasia) {
+        this.fantasia = fantasia;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public List<AtividadeDTO> getNatureza_juridica() {
+        return natureza_juridica;
+    }
+
+    public void setNatureza_juridica(List<AtividadeDTO> natureza_juridica) {
+        this.natureza_juridica = natureza_juridica;
+    }
+
+    public List<AtividadeDTO> getAtividades_secundarias() {
+        return atividades_secundarias;
+    }
+
+    public void setAtividades_secundarias(List<AtividadeDTO> atividades_secundarias) {
+        this.atividades_secundarias = atividades_secundarias;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getData_situacao() {
+        return data_situacao;
+    }
+
+    public void setData_situacao(String data_situacao) {
+        this.data_situacao = data_situacao;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getUltima_atualizacao() {
+        return ultima_atualizacao;
+    }
+
+    public void setUltima_atualizacao(String ultima_atualizacao) {
+        this.ultima_atualizacao = ultima_atualizacao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEfr() {
+        return efr;
+    }
+
+    public void setEfr(String efr) {
+        this.efr = efr;
+    }
+
+    public String getMotivo_situacao() {
+        return motivo_situacao;
+    }
+
+    public void setMotivo_situacao(String motivo_situacao) {
+        this.motivo_situacao = motivo_situacao;
+    }
+
+    public String getSituacao_especial() {
+        return situacao_especial;
+    }
+
+    public void setSituacao_especial(String situacao_especial) {
+        this.situacao_especial = situacao_especial;
+    }
+
+    public String getData_situacao_especial() {
+        return data_situacao_especial;
+    }
+
+    public void setData_situacao_especial(String data_situacao_especial) {
+        this.data_situacao_especial = data_situacao_especial;
+    }
+
+    public String getCapital_social() {
+        return capital_social;
+    }
+
+    public void setCapital_social(String capital_social) {
+        this.capital_social = capital_social;
+    }
+
+    public QsaDTO getQsa() {
+        return qsa;
+    }
+
+    public void setQsa(QsaDTO qsa) {
+        this.qsa = qsa;
+    }
+
+    public BillingDTO getBilling() {
+        return billing;
+    }
+
+    public void setBilling(BillingDTO billing) {
+        this.billing = billing;
+    }
 }
 
 class AtividadeDTO implements Serializable {
